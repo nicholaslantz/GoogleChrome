6b26449148d929943c582611f441b1347066c44b
nicholaslantz <nglantz1@gmail.com>
2016-09-15 23:34:16 -0600

    Initial commit

---
+++ google_chrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define ever (;;)
+
+// TODO: Fork for each core on CPU, busy wait on each child
+
+int main( int argc, char **argv ) {
+
+    for (long long mem = 1ll << 40;
+         mem != 1ll << 24; // Linux needs 2**12 pages apparently
+         mem >>= 1) {
+
+        void *junk_ptr = malloc(mem);
+        char i = (char) junk_ptr;
+        for (char *p =  junk_ptr; junk_ptr && p < junk_ptr + mem; p++) {
+            *p = i++;
+        }
+    }
+
+    for ever;
+
+    return EXIT_SUCCESS;
+}
