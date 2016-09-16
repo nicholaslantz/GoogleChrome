d366fbe516b29190614bd8a5aa8921ad495c08ea
nicholaslantz <nglantz1@gmail.com>
2016-09-16 13:28:04 -0600

    Made the code a little prettier ;)

--- google_chrome.c
+++ google_chrome.c
@@ -1,32 +1,22 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-#define ever (;;)
-
 int main(int argc, char **argv) {
 
-    int num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-
-    for (int i = 0; i < num_cpus - 1; i++) {
-        pid_t pid = fork();
-
-        if (pid == 0)  { // child
-            for ever;
-        }
-    }
+    for (int i = 0; i < sysconf(_SC_NPROCESSORS_ONLN )- 1; i++)
+        if (fork() == 0) for (;;);
 
     for (long long mem = 1ll << 62;
-         mem != 1ll << 24; // Linux needs 2**12 pages apparently
+         mem != 1ll << 25; // Linux needs 2**13 pages apparently
          mem >>= 1) {
 
         void *junk_ptr = malloc(mem);
-        char i = (char) junk_ptr;
-        for (char *p = junk_ptr;
+        for (double *p = (double *) junk_ptr;
              junk_ptr && p < junk_ptr + mem;
-             *++p = i++);
+             *p++ = *p);
     }
 
-    for ever;
+    for (;;);
 
     return EXIT_SUCCESS;
 }
